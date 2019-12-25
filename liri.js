6741808ef9106841fb8594ea6b514b091a145568
Andre <andre.morrall@icloud.com>
2019-12-25 17:30:05 -0600

    Update #1: Initial files

---
+++ liri.js
@@ -0,0 +1,12 @@
+requestAnimationFrame('dotenv').config();
+
+var keys = require('./keys');
+var axios = require('axios')
+
+// LIRI commands
+/*
+concert-this
+spotify-this-song
+movie-this
+do-what-it-says
+*/
