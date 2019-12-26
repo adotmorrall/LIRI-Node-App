db10d8e6f1884c6343f542cbd171a3d6ef2d4c5e
Andre <andre.morrall@icloud.com>
2019-12-26 16:17:59 -0600

    Update #5: Removed concert-this, spotify-this-song, and movie-this JS files. Moved content into liri.js

--- liri.js
+++ liri.js
@@ -1,22 +1,76 @@
 require("dotenv").config();
 
+var axios = require('axios');
+// var moment = require('moment');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-var axios = require('axios');
-
 var spotify = new Spotify(keys.spotify);
+// var fs = require('fs');
 
-// liri.js spotify-this-song
-// function userSongSearch(song) {
-//     spotify.search({ type: 'track', query: song || 'The Sign Ace of Base' }, function (err, data) {
-//         if (err) {
-//             return console.log('Error occurred: ' + err);
-//         }
+var liriCall = process.argv[2];
+var userInput = process.argv.splice(3, process.argv.length).join(' ');
 
-//         console.log(data.tracks.items[0]);
+function userQuery(liriCall, userInput) {
+    switch (liriCall) {
+        case 'spotify-this-song':
+            getSpotifySong();
+            break;
+        case 'movie-this':
+            getMovie();
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+    }
+}
+userQuery(liriCall, userInput);
 
-//     });
-// };
+
+// spotify-this-song command
+function getSpotifySong() {
+    var song = userInput;
+    spotify.search({ type: 'track', query: song || 'The Sign Ace of Base' }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var output = data.tracks.items[0];
+
+        // console.log(song);
+        console.log('                                   ');
+        console.log('Artist: ' + output.artists[0].name);
+        console.log('Track: ' + output.name);
+        console.log('Track preview: ' + output.preview_url);
+        console.log('Album: ' + output.album.name);
+
+    });
+};
+
+// // movie-this command
+function getMovie() {
+    var movie = userInput;
+    if (!movie) {
+        movie = 'Mr. Nobody'
+    }
+    var queryURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=trilogy'; ~
+        axios.get(queryURL)
+            .then(function (response) {
+                var omdbInfo = response.data
+                // console.log(omdbInfo);
+                console.log(userInput);
+                console.log('                                               ');
+                console.log('Title: ' + omdbInfo.Title);
+                console.log('Year: ' + omdbInfo.Year);
+                console.log('IMDB Rating: ' + omdbInfo.imdbRating);
+                console.log('Rotten Tomatoes Rating: ' + omdbInfo.Ratings[1].Value);
+                console.log('Country: ' + omdbInfo.Country);
+                console.log('Language: ' + omdbInfo.Language);
+                console.log('Actors: ' + omdbInfo.Actors);
+                console.log('Plot: ' + omdbInfo.Plot);
+            });
+};
+
+
+
 
 // liri.js concert-this
 // function searchBand(band) {
@@ -39,10 +93,3 @@ var spotify = new Spotify(keys.spotify);
 // };
 // console.log(process.argv.slice(3).join(' '))
 // console.log(process.argv[3])
-
-// function commandHandler(arg, command) {
-//     if (command === 'spotify-this-song') {
-//         userSongSearch(arg);
-//     }
-// };
-// commandHandler('Hotel California', 'spotify-this-song');
